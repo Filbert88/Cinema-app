02127b2f70edc013b0aa244fb5e1182ca758aed3
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-06-19 17:42:38 +0700

    feat: add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        red: "#E50914",
+        black: "#141414"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -17,4 +21,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
