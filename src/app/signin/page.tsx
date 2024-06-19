2970bd744945e3507a910b41366f9ec01827d255
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-06-19 23:20:33 +0700

    feat: add signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,8 @@
+import Signin from '@/components/signin'
+import React from 'react'
+
+export default function SingInPage() {
+  return (
+    <div><Signin /></div>
+  )
+}
---
+++ src/components/signin.tsx
@@ -0,0 +1,113 @@
+"use client"
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";  
+
+interface Errors {
+  email: string;
+  password: string;
+}
+
+interface Touched {
+  email: boolean;
+  password: boolean;
+}
+
+const Signin: React.FC = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [errors, setErrors] = useState<Errors>({ email: "", password: "" });
+  const [touched, setTouched] = useState<Touched>({ email: false, password: false });
+
+  const handleBlur = (field: keyof Touched): void => {
+    setTouched({ ...touched, [field]: true });
+    if (field === "email") {
+      setErrors({ ...errors, email: validateEmail(email) });
+    } else {
+      setErrors({ ...errors, password: validatePassword(password) });
+    }
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const validateEmail = (email: string): string => {
+    if (!email) {
+      return "Email is required.";
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      return "Please enter a valid email.";
+    }
+    return "";
+  };
+
+  const validatePassword = (password: string): string => {
+    if (!password) {
+      return "Password is required.";
+    } else if (password.length < 4 || password.length > 60) {
+      return "Your password must contain between 4 and 60 characters.";
+    }
+    return "";
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    
+    router.push('/'); 
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <div className="text-4xl mb-6">Sign In</div>
+        <div className="flex flex-col space-y-6 font-normal items-center">
+          <input
+            placeholder="Email"
+            className="py-4 px-5 w-[350px] text-black rounded-md"
+            value={email}
+            onChange={handleEmailChange}
+            onBlur={() => handleBlur("email")}
+            required
+          />
+          {touched.email && errors.email && <div className="text-red-500 text-xs w-[350px] mt-1">{errors.email}</div>}
+          <div className="relative w-[350px]">
+            <input
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+              className="py-4 px-5 w-full text-black rounded-md"
+              value={password}
+              onChange={handlePasswordChange}
+              onBlur={() => handleBlur("password")}
+              required
+            />
+            {password && (
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={togglePasswordVisibility}>
+                <Image src="/visible.svg" alt="Toggle visibility" width={20} height={20} />
+              </div>
+            )}
+          </div>
+          {touched.password && errors.password && <div className="text-red-500 text-xs w-[350px] mt-1">{errors.password}</div>}
+        </div>
+        <button type="submit" className="mt-4 rounded-lg py-3 px-5 w-[350px] text-white bg-red">
+          Sign In
+        </button>
+        <div className="flex flex-row space-x-2 mt-2">
+          <div className="text-gray-300">New here?</div>
+          <a className="hover:underline hover:cursor-pointer" onClick={() => router.push("/signup")}>Sign up now.</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
